ef3414848cbdbf42d29d6e387753b39111b73c3d
Wendel Côrtes Martins <wcmestre@hotmail.com>
2021-01-21 12:07:47 -0300

    Adicionar arquivos de projeto.

---
+++ Estruturas de Repetição/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Estruturas_de_Repetição
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int qtde, cont, numero, resultado=0;
+
+            Console.Write("Quantos números você quer somar? ");
+            qtde = int.Parse(Console.ReadLine());
+
+            for (cont = 1; cont <= qtde; cont ++)
+            {
+                Console.Write("Digite o número " + cont + " : ");
+                numero = int.Parse(Console.ReadLine());
+                resultado = resultado + numero;
+            }
+
+            Console.Write("\nO resultado é: " + resultado);
+
+            Console.Write("\nPressione qualquer tecla para continuar . . . ");
+            Console.ReadKey(true);
+        }
+    }
+}
