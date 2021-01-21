03c36628823aecaef154a55072aa364990be1de1
Wendel Côrtes Martins <wcmestre@hotmail.com>
2021-01-21 12:13:09 -0300

    while

--- Estruturas de Repetição/Program.cs
+++ Estruturas de Repetição/Program.cs
@@ -10,16 +10,26 @@ namespace Estruturas_de_Repetição
     {
         static void Main(string[] args)
         {
-            int qtde, cont, numero, resultado=0;
+            int qtde, cont=1, numero, resultado=0;
 
             Console.Write("Quantos números você quer somar? ");
             qtde = int.Parse(Console.ReadLine());
 
+            /*
             for (cont = 1; cont <= qtde; cont ++)
             {
                 Console.Write("Digite o número " + cont + " : ");
                 numero = int.Parse(Console.ReadLine());
                 resultado = resultado + numero;
+            }
+            */
+
+            while (cont <= qtde)
+            {
+                Console.Write("Digite o número " + cont + " : ");
+                numero = int.Parse(Console.ReadLine());
+                resultado = resultado + numero;
+                cont++;
             }
 
             Console.Write("\nO resultado é: " + resultado);
